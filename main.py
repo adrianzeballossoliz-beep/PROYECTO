4c75258769e44abc53d505b4cdf0f4ca81e3d749
adrianzeballossoliz-beep <adrianzeballossoliz@gmail.com>
2026-03-26 11:24:35 -0400

    vv

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import numpy as np
 # =====================================================
 
 st.set_page_config(
-    page_title="Horizon Stay | Business Intelligence PRO",
+    page_title="Horizon Stay ",
     page_icon="🏨",
     layout="wide",
     initial_sidebar_state="expanded"
